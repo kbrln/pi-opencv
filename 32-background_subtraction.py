bfad14388841623c4e896ed761d0b2f78700c97d
Nissan Silvia <200sxmeda@gmail.com>
2020-06-26 23:27:07 +1000

    minor changes

--- 32-background_subtraction.py
+++ 32-background_subtraction.py
@@ -8,10 +8,10 @@ import cv2 as cv
 cap = cv.VideoCapture('vtest.avi')
 # kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3)) # needed for GMG
 
-# fgbg = cv.bgsegm.createBackgroundSubtractorMOG() # foreground background 
+fgbg = cv.bgsegm.createBackgroundSubtractorMOG() # foreground background 
 # fgbg = cv.createBackgroundSubtractorMOG2(detectShadows=False) # another method
 # fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
-fgbg = cv.createBackgroundSubtractorKNN(detectShadows=False) # 
+# fgbg = cv.createBackgroundSubtractorKNN(detectShadows=False) # 
 
 while True: 
     ret, frame = cap.read()
